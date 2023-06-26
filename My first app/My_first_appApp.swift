1de61a84481f0d5651ec52ad0f4b4cdcd1aa2e06
Jasmitha Alle <allejasmitha@Gmail.com>
2023-06-26 10:30:34 -0400

    Initial Commit

---
+++ My first app/My_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_first_appApp.swift
+//  My first app
+//
+//  Created by Jasmitha Alle on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct My_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
